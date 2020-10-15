e4e7d9e9855d3984bfaef5e8f75106939446f123
Filip Sedlák <hyppercrone@gmail.com>
2020-10-16 00:28:56 +0200

    Animation working

--- TheGame/GameStuff/Animation.cs
+++ TheGame/GameStuff/Animation.cs
@@ -7,40 +7,58 @@ namespace TheGame.GameStuff
 {
   class Animation
   {
-    private float start;
-    private float last;
     private float delta;
+    private int defaultFrame;
 
-    public List<Texture2D> Frames { get; set; }
+    public Texture2D Sprite;
     public float Frequency { get; set; }
     public int CurrentFrameIndex { get; set; }
-    public Texture2D CurrentFrame => Frames[CurrentFrameIndex];
-    public int FrameCount => Frames.Count;
-    public Animation(List<Texture2D> frames, float frequency)
+    public int width;
+    public int heigth;
+    public int frameCount;
+    public bool Active { get; private set; }
+    public Animation(Texture2D sprite, float frequency, int frameCount, int wid, int hei, int defaultFrame = 0)
     {
-      Frames = frames;
+      Sprite = sprite;
       Frequency = frequency;
+      this.frameCount = frameCount;
+      width = wid;
+      heigth = hei;
+      Active = false;
+      this.defaultFrame = defaultFrame;
+      CurrentFrameIndex = defaultFrame;
     }
 
     public void Update(UpdateArguments arguments)
     {
+      if (!Active) return;
+
       delta += (float)arguments.Time.ElapsedGameTime.TotalMilliseconds;
 
       if (delta >= Frequency)
       {
-        CurrentFrameIndex = (CurrentFrameIndex + 1) % FrameCount;
+        CurrentFrameIndex = (CurrentFrameIndex + 1) % frameCount;
         delta -= Frequency;
       }
     }
 
+    public void Render(RenderArguments arguments, Vector2 pos, Color color, int column)
+    {
+      arguments.SpriteBatch.Draw(Sprite, pos, new Rectangle(CurrentFrameIndex * width, heigth * column, width, heigth), color);
+    }
+
     public void Stop()
     {
-
+      Active = false;
+      CurrentFrameIndex = defaultFrame;
+      delta = 0;
     }
 
     public void Restart()
     {
-
+      Active = true;
+      delta = 0;
+      CurrentFrameIndex = defaultFrame;
     }
   }
 }
--- TheGame/GameStuff/Entities/Player.cs
+++ TheGame/GameStuff/Entities/Player.cs
@@ -11,19 +11,18 @@ namespace TheGame.GameStuff.Entities
 {
   class Player : Entity
   {
-    
-
-
     Vector2 Velocity = new Vector2(1.5f, 1.5f);
     float speed = 300f;
     private Vector2 Movement;
-    private Animation animUp, animLeft, animDown, animRight;
-    private Animation currAnim;
+    private enum direction { up = 0, left = 1, down = 2, right = 3 };
+    private direction dir = direction.up;
+    private Animation animation;
 
     public Player(World world) : base(world)
     {
       Width = 32;
       Height = 32;
+      animation = new Animation(Assets.PlayerSprite, 85, 4, 32, 32, 1);
     }
 
     public override void Render(RenderArguments arguments)
@@ -33,7 +32,8 @@ namespace TheGame.GameStuff.Entities
         (int)Position.Y + 16
         );
       Point pos = Camera.AbsoluteToRelative(Position.ToPoint());
-      arguments.SpriteBatch.Draw(Assets.placeHolder, pos.ToVector2(), Color.White);
+
+      animation.Render(arguments, pos.ToVector2(), Color.White, (int)dir);
     }
 
     public override void Update(UpdateArguments arguments)
@@ -41,6 +41,7 @@ namespace TheGame.GameStuff.Entities
       if (arguments.Keyboard.GetPressedKeys().Count() == 0)
       {
         Action = null;
+        animation.Stop();
         return;
       }
 
@@ -49,26 +50,34 @@ namespace TheGame.GameStuff.Entities
       if (arguments.Keyboard.IsKeyDown(Keys.W))
       {
         Velocity += CommonVectors.Up;
-        currAnim = animUp;
-      }
-      if (arguments.Keyboard.IsKeyDown(Keys.A))
-      {
-        Velocity += CommonVectors.Left;
-        currAnim = animLeft;
+        dir = direction.up;
       }
       if (arguments.Keyboard.IsKeyDown(Keys.S))
       {
         Velocity += CommonVectors.Down;
-        currAnim = animDown;
+        dir = direction.down;
+      }
+      if (arguments.Keyboard.IsKeyDown(Keys.A))
+      {
+        Velocity += CommonVectors.Left;
+        dir = direction.left;
       }
       if (arguments.Keyboard.IsKeyDown(Keys.D))
       {
         Velocity += CommonVectors.Right;
-        currAnim = animRight;
+        dir = direction.right;
       }
 
       if (Velocity.Length() != 0)
+      {
         Velocity.Normalize();
+        if (!animation.Active)
+          animation.Restart();
+      }
+      else
+        animation.Stop();
+
+      animation.Update(arguments);
 
       Movement = Velocity * speed * (float)arguments.Time.ElapsedGameTime.TotalSeconds;
 
